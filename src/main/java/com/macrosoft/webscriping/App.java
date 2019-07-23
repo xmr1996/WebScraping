2803bb199c8651ce49a7febb505fa05c3f8b0ccb
Mingrem Xiang <mxiang@macrosoftinc.com>
2019-07-23 15:29:12 -0400

    find last modified date of the given Url

--- src/main/java/com/macrosoft/webscriping/App.java
+++ src/main/java/com/macrosoft/webscriping/App.java
@@ -1,9 +1,15 @@
 package com.macrosoft.webscriping;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * Hello world!
@@ -13,15 +19,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        String testUrl = "https://aws.amazon.com/solutions/ai-powered-speech-analytics-for-amazon-connect/";
         
-        try {
-			Document doc = Jsoup.connect(testUrl).get();
-			System.out.println(doc.outerHtml());
+        String test[] = {"https://aws.amazon.com/quickstart/connect/callminer-eureka/",
+        		"https://aws.amazon.com/lambda/features/",
+        		"https://register.callminer.net/Content/Documents/callMiner-amazon-connect-integration-manual.pdf",
+        		"https://introcs.cs.princeton.edu/java/home/chapter1.pdf",
+        		"https://www.cs.cmu.edu/afs/cs.cmu.edu/user/gchen/www/download/java/LearnJava.pdf"};
+      
+    	
+        
+    	try {
+    		for(String url:test) {
+    			Connection.Response result = Jsoup.connect(url).ignoreContentType(true).execute();
+    			Document htmlDoc = Jsoup.connect(url).ignoreContentType(true).get();
+    			String fileTitle = htmlDoc.title();
+    			System.out.println("\"" + fileTitle + "\" is last modified in " + result.header("Last-Modified"));
+    		}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
     }
+    
+
 }
