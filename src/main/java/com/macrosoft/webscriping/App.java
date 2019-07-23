96472b7bfb32c20d4646e2dc067e2df9dfd33de6
Mingrem Xiang <mxiang@macrosoftinc.com>
2019-07-23 13:04:09 -0400

    testing

--- src/main/java/com/macrosoft/webscriping/App.java
+++ src/main/java/com/macrosoft/webscriping/App.java
@@ -1,5 +1,10 @@
 package com.macrosoft.webscriping;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String testUrl = "https://aws.amazon.com/solutions/ai-powered-speech-analytics-for-amazon-connect/";
+        
+        try {
+			Document doc = Jsoup.connect(testUrl).get();
+			System.out.println(doc.outerHtml());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
     }
 }
